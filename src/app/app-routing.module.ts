165b6f217fb6c7d4b6c9b2555d778bd373ed581a
Streeterxs <afonso_mg3@hotmail.com>
2020-09-16 16:40:53 -0300

    Architecture - Lazy Loading: Added lazy loading for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+
 const routes: Routes = [
   {
     path: '',
     children: [
       {
-        path: '/character',
+        path: 'character',
         loadChildren: () => import('./modules/characters/characters.module').then(m => m.CharactersModule)
       },
       {
         path: '**',
-        redirectTo: '/character'
+        pathMatch: 'full',
+        redirectTo: 'character'
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'character'
       }
     ]
   }
--- src/app/modules/characters/characters-routing.module.ts
+++ src/app/modules/characters/characters-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharactersComponent } from './characters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CharactersComponent,
+    children: [
+      {
+        path: 'list',
+        loadChildren: () => import('./list/list.module').then(m => m.ListModule)
+      },
+      {
+        path: '',
+        redirectTo: 'list'
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
